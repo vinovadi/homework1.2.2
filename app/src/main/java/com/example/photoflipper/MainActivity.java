e8733036c7d1dae995116d5cea2a4161e7e5946d
vinovadi <v.vinokhodov@gmail.com>
2019-08-16 17:14:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/photoflipper/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.photoflipper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Random random = new Random();
+        final TextView txtLink = findViewById(R.id.txtLink);
+        txtLink.setText("http://myfile.org/" + random.nextInt(100));
+        Button btnNext = findViewById(R.id.btnNext);
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
